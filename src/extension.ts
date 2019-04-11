dc5c185c120d254fb0241086ab86dee862c59cf8
Jonny Hork <jhork@salesforce.com>
2019-04-11 11:29:57 -0600

    Clean up comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "saywhat" is now active!');
 
   let getMessage = vscode.commands.registerCommand(
@@ -15,29 +9,26 @@ export function activate(context: vscode.ExtensionContext) {
     async () => {
       // The code you place here will be executed every time your command is executed
       let message = await vscode.window.showInputBox();
-      console.log(message);
-
-      return message;
+      console.log("Logged from native sayWhat ext-> ", message);
     }
   );
+  // might need to add this in sayHi
+  context.subscriptions.push(getMessage);
 
   let getMessageFn = async () => {
     let message = await vscode.window.showInputBox();
-    console.log(message);
+    console.log("Logged from imported getMessageFn-> ", message);
 
     return message;
   };
-  // might need to add this in sayHi
-  context.subscriptions.push(getMessage);
 
   let api: any = {
     getMessageFn
   };
-  // 'export' public api-surface
+  // 'exports' public api-surface
   return api;
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
   console.log("Say what is deactivated!");
 }
